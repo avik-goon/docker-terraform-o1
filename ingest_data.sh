d246ae39796281efcb760b13a0a2f12f318e12e9
Avik Goon <avikgoon@icloud.com>
2025-03-09 20:32:19 +0000

    docker yml configured to save pg admin data and use cutom network

--- ingest_data.sh
+++ ingest_data.sh
@@ -7,7 +7,7 @@ docker run -it \
   taxi_ingest:v001 \
     --user=root \
     --password=root \
-    --host=pg-database \
+    --host=pgdatabase \
     --port=5432 \
     --db=ny_taxi \
     --table_name=yellow_taxi_data \
